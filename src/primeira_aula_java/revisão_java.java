fba70270318dae70d20819c9a8db0e01e67d8ff5
lcsdiasferreira3 <lcsdferrira3@gmail.com>
2019-02-14 10:38:27 -0200

    primeira_aula

---
+++ src/primeira_aula_java/revisão_java.java
@@ -0,0 +1,45 @@
+package primeira_aula_java;
+
+public class revisão_java {
+
+/*
+public int soma(int a, int b) {
+	return a + b;		
+}	
+	
+public void imprimir( ) {
+	System.out.println("Teste");
+}
+*/	
+
+	
+	
+	public void tabuada (int a) {
+		for(int x=0; x <=10; x++) {
+		System.out.println(x * a);
+		
+	}
+	}
+	
+	public static void main(String[] args) {
+
+ revisão_java obj = new  revisão_java();
+/* System.out.println(obj.soma(10, 5) );
+ */
+ //obj.imprimir(5);
+ obj.tabuada(8);
+	
+
+
+ 
+
+	
+/*int x = 2; 
+int y = 3;
+
+System.out.println(y + x);  
+System.out.println("Olá mundo");*/
+
+ }
+
+}
